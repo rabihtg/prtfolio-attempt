98f7cebe217cade0aebf08565fa4b94b914be3ed
rabih harb <rabiharb.ss@outlook.com>
2022-03-21 19:59:46 +0200

    add horizant wheel scrolling, keyboard navigation for gallery(needs fixing)

--- assets/js/gallery-loader.js
+++ assets/js/gallery-loader.js
@@ -1,9 +1,21 @@
 const galleryCont = document.querySelector(".work__gallery");
+
+let galleryItemAnchors,
+  // Flipped because of grid-auto-flow: column;
+  nbOfCol = 3,
+  nbOfRow = 7,
+  resetIndex = 20,
+  currentAnchorIndex = 0,
+  galleryContCoor = {
+    left: 0,
+    x: 0,
+  };
+
+export { galleryLoader, galleryKeyNavigator, gallerySlider };
 import { data } from "../gallery_items.js";
 
-export function galleryLoader() {
-  let href;
-  let src;
+function galleryLoader() {
+  let href, src;
   Object.keys(data).forEach((item) => {
     let galleryItem = document.createElement("div");
     let itemAnchor = document.createElement("a");
@@ -18,6 +30,7 @@ export function galleryLoader() {
     itemAnchor.setAttribute("href", href);
     itemAnchor.setAttribute("rel", "noopener noreferrer");
     itemAnchor.setAttribute("target", "_blank");
+    itemAnchor.setAttribute("tabindex", "-1");
 
     src = `./assets/images/gallery/${data[item].imgSrc}`;
     itemImg.setAttribute("src", src);
@@ -39,4 +52,74 @@ export function galleryLoader() {
     textCont.appendChild(itemTitle);
     textCont.appendChild(itemDesc);
   });
+  const firsAnchor = document.querySelector(".gallery-item:first-of-type > a");
+  galleryItemAnchors = document.querySelectorAll(".gallery-item > a");
+  firsAnchor.setAttribute("tabindex", "0");
+}
+
+function galleryKeyNavigator() {
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 769) {
+      nbOfRow = 5;
+      nbOfCol = 4;
+    } else {
+      nbOfRow = 3;
+      nbOfCol = 7;
+      resetIndex = 20;
+    }
+  });
+  let col = 0,
+    row = 0;
+  galleryCont.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "ArrowRight":
+        row += 1;
+        if (row > resetIndex) {
+          row = 0;
+        }
+        break;
+      case "ArrowLeft":
+        row -= 1;
+        if (row < 0) {
+          row = galleryItemAnchors.length - 1;
+        }
+        break;
+      case "ArrowDown":
+        col += 1;
+        if (col > resetIndex) {
+          col = 0;
+        }
+        break;
+      case "ArrowUp":
+        col -= 1;
+        if (col < 0) {
+          col = galleryItemAnchors.length - 1;
+        }
+        break;
+    }
+    if (e.key != "Enter" && e.key != "Tab") {
+      e.preventDefault();
+    }
+    currentAnchorIndex = (nbOfCol * row + col) % (nbOfCol * nbOfRow - 1);
+    galleryItemAnchors[currentAnchorIndex].focus();
+    console.log(row, col, currentAnchorIndex);
+    document
+      .querySelector(".gallery-item > a[tabindex='0']")
+      .setAttribute("tabindex", "-1");
+    galleryItemAnchors[currentAnchorIndex].setAttribute("tabindex", "0");
+
+    galleryItemAnchors[currentAnchorIndex].scrollIntoView({
+      inline: "center",
+      block: "center",
+    });
+  });
+}
+
+function gallerySlider() {
+  galleryCont.addEventListener("wheel", mouseWheelHandler);
+}
+
+function mouseWheelHandler(e) {
+  e.preventDefault();
+  galleryCont.scrollLeft -= e.deltaY;
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
+// Todo - user-select: none on gallery-item txt (mobile-devices).
+// Todo - add functionality to see all in loopstudio website.
+// Todo - make home page loading page..add functionality to check selected page and link selected btn acconrdingly.
+// Todo - fix font-size for gallery-item txt.
+// Todo - re-order gallery-item.js data.
+// Todo - check section height code.
+// Todo - Fix row & col trace in galleryLoader !important caught bug.
+
 "use strict";
 const animatedElms = document.querySelectorAll("[data-animated='true'");
 const homeSection = document.querySelector("#home");
 import { startDragSlider } from "./languageSlider.js";
 import { sectionSwitcher } from "./change-section.js";
 import { startTxtType } from "./typing-txt-animator.js";
-import { galleryLoader } from "./gallery-loader.js";
+import {
+  galleryLoader,
+  galleryKeyNavigator,
+  gallerySlider,
+} from "./gallery-loader.js";
 
 window.addEventListener("load", () => {
   animatedElms.forEach((elm) => {
@@ -18,4 +30,6 @@ window.addEventListener("load", () => {
     sectionSwitcher();
   }, 3000);
   galleryLoader();
+  galleryKeyNavigator();
+  gallerySlider();
 });
